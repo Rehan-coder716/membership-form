b9a70b5a8cb22223fefcb3c3d97ac13b210cadbb
urmirahman.shila944@gmail.com <urmirahman.shila944@gmail.com>
2024-10-25 10:28:40 +0000

    Created ServerModule1; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,9 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 
 
 class Form1(Form1Template):
@@ -8,3 +12,13 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def outlined_button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    name = self.text_box_1.text
+    weight = int(self.text_box_2.text)
+    address = self.text_area_1.text
+    personal = self.check_box_1.checked
+    email = self.text_box_3.text
+    anvil.server.call('submit', name=name, weight=weight, address=address, personal=personal, email=email )
+    Notification("Your response has been recorded").show()
